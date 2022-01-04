3d0e23d085c778f2ffb3c905020f30cb8390e3f1
Asrbek <87761511+AsrbekCoder@users.noreply.github.com>
2022-01-04 19:44:36 +0500

    happy hacking !!!

--- src/Js/js.js
+++ src/Js/js.js
@@ -6,24 +6,26 @@ const btnCopy = document.querySelector(".btn_copy");
 const doneMes = document.querySelector(".done_message");
 
 inputValue.addEventListener("change", (e) => {
-  let givenWords = e.target.value
-    .split(" ")
-    .map((e) => e[0])
-    .join("")
-    .toUpperCase();
+    let givenWords = e.target.value
+        .split(" ")
+        .map((e) => e[0])
+        .join("")
+        .toUpperCase();
 
-  if (e.target.value) {
-    loader.style.display = "none";
-    text.innerText = givenWords;
-    btn.style.opacity = "1";
-  } else {
-    text.innerText = "";
-    loader.style.display = "inherit";
-  }
-});
+    if (e.target.value.length > 0) {
+        loader.style.display = "none";
+        text.innerText = givenWords;
+        btn.style.opacity = "1";
 
-btnCopy.addEventListener("click", () => {
-  doneMes.style.left = "50%";
-  navigator.clipboard.writeText(text.innerText);
-  doneMes.innerText = `Kопия сделана:${text.innerText}`;
+        btnCopy.addEventListener("click", () => {
+            doneMes.style.left = "50%";
+            let promise = navigator.clipboard.writeText(text.innerText);
+            doneMes.innerText = `Kопия сделана:${text.innerText}`;
+        });
+    } else {
+        text.innerText = "";
+        loader.style.display = "inherit";
+        doneMes.style.display = "none"
+        btnCopy.style.display = "none"
+    }
 });
